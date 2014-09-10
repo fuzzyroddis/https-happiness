b0333c59845f4ea412c6573e72fcbbd6a7d10986
stevenroddis <stevenroddis@users.noreply.github.com>
2014-09-10 22:17:53 +1000

    Regex should work but why isn't it?

--- mkrules.php
+++ mkrules.php
@@ -7,6 +7,7 @@
 */
 function process_line($line)
 {
+	echo($line);
 	//Might as well have fun with this! Some call it job security.
 	preg_match("%^(.+)			#name
 				\s\|\s
@@ -21,24 +22,31 @@ function process_line($line)
 				$line, $matches);
 	list(, $NAME, $PATTERN, $wild, $HOST, $REWRITE) = $matches;
 
-	$rule = "<ruleset name=\"{$NAME}\">\r\n  <target host=\"{$HOST}\" />";
-
-	if($wild)
+	if($NAME AND $PATTERN AND $HOST AND $REWRITE)
 	{
-		$rule += "\r\n <target host=\"*.{$HOST}\" />";
+		$rule = "<ruleset name=\"{$NAME}\">\r\n  <target host=\"{$HOST}\" />";
+
+		if($wild)
+		{
+			$rule += "\r\n <target host=\"*.{$HOST}\" />";
+		}
+
+		if(substr($PATTERN, -1, 1) === '/')
+			$PATTERN = substr($PATTERN, 0, -1);
+
+		if(substr($REWRITE, -1, 1) === '/')
+			$REWRITE = substr($REWRITE, 0, -1);
+
+		$rule += "\r\n  <rule from=\"^http://{$PATTERN}/\" to=\"{$REWRITE}/\"/>\r\n</ruleset>";
+
+		$filename = preg_replace("/[^a-zA-Z0-9\-\_\.]/", '', $NAME);
+
+		file_put_contents($filename, 'rules/'.$rule);
 	}
-
-	if(substr($PATTERN, -1, 1) === '/')
-		$PATTERN = substr($PATTERN, 0, -1);
-
-	if(substr($REWRITE, -1, 1) === '/')
-		$REWRITE = substr($REWRITE, 0, -1);
-
-	$rule += "\r\n  <rule from=\"^http://{$PATTERN}/\" to=\"{$REWRITE}/\"/>\r\n</ruleset>";
-
-	$filename = preg_replace("/[^a-zA-Z0-9\-\_\.]/", '', $NAME);
-
-	file_put_contents($filename, 'rules/'.$rule);
+	else
+	{
+		echo 'Error processing line';
+	}
 }
 
 
