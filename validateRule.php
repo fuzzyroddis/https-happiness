2a7b8d19976bedb93519c0adbfce6b3243bcec7f
stevenroddis <stevenroddis@users.noreply.github.com>
2014-09-20 15:59:35 +1000

    Added tests for missing question mark #9

--- validateRule.php
+++ validateRule.php
@@ -11,14 +11,18 @@ if($filename)
 	foreach($rules as $rule)
 	{
 		$from = $rule->getAttribute("from");
+		$to = $rule->getAttribute("to");
+		if(preg_match('%www\.\)[^?]%', $from)) //http://(?:www\.)example\.com
+		{
+			echo "FAILED (missing question mark): ".$doc->saveXML($rule)."\n";
+		}
 		if(strpos($from, "\.)\.") !== false) //http://(?:www\.)\.example\.com
 		{
 			echo "FAILED (extra dot): ".$doc->saveXML($rule)."\n";
 		}
-		$to = $rule->getAttribute("to");
 		if(preg_match("%".$from."%", $to, $matches) === FALSE) //I don't feel confortable with addslashes, I worry it might fix errors
 		{
-			echo "FAILED: ".$doc->saveXML($rule)."\n";
+			echo "FAILED: (bad regex)".$doc->saveXML($rule)."\n";
 		}
 
 	}
